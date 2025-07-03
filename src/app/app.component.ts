c4aa11814eb66fdc911815e284ebcc8079e52867
Emmanuel ahiabor <ahiaboremmanuel9@gmail.com>
2025-07-03 17:44:47 +0000

    Ajout de la musique

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'ldc';
+  isAudioPlaying = false;
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngAfterViewInit() {
+    const audio = document.getElementById('background-audio') as HTMLAudioElement;
+    if (audio) {
+      audio.addEventListener('play', () => {
+        this.isAudioPlaying = true;
+        this.cdr.detectChanges();
+      });
+      audio.addEventListener('pause', () => {
+        this.isAudioPlaying = false;
+        this.cdr.detectChanges();
+      });
+      audio.addEventListener('ended', () => {
+        this.isAudioPlaying = false;
+        this.cdr.detectChanges();
+      });
+    }
+  }
+
+  toggleAudio() {
+    const audio = document.getElementById('background-audio') as HTMLAudioElement;
+    if (audio) {
+      if (audio.paused) {
+        audio.play();
+      } else {
+        audio.pause();
+      }
+    }
+  }
 }
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit, ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
-export class MainComponent {
+export class MainComponent implements AfterViewInit {
+  @ViewChild('audioPlayer', { static: false }) audioPlayer!: ElementRef<HTMLAudioElement>;
+  isAudioPlaying = false;
 
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngAfterViewInit() {
+    const audio = this.audioPlayer.nativeElement;
+    audio.addEventListener('play', () => {
+      this.isAudioPlaying = true;
+      this.cdr.detectChanges();
+    });
+    audio.addEventListener('pause', () => {
+      this.isAudioPlaying = false;
+      this.cdr.detectChanges();
+    });
+    audio.addEventListener('ended', () => {
+      this.isAudioPlaying = false;
+      this.cdr.detectChanges();
+    });
+  }
+
+  toggleAudio() {
+    const audio: HTMLAudioElement = this.audioPlayer.nativeElement;
+    if (audio.paused) {
+      audio.play();
+    } else {
+      audio.pause();
+    }
+    // isAudioPlaying sera mis à jour automatiquement par les listeners
+  }
 }
