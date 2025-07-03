82939e8843254a08d4c043bf1defe07829bcd0b2
Emmanuel ahiabor <ahiaboremmanuel9@gmail.com>
2025-07-03 16:14:57 +0000

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { ProjetsComponent } from './pages/projets/projets.component';
+import { ResumeComponent } from './pages/resume/resume.component';
+import { WebsitesComponent } from './pages/websites/websites.component';
+import { ServicesComponent } from './pages/services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainComponent },
+  {path: 'about', component: AboutComponent },
+  {path: 'contacts', component: ContactsComponent },
+  {path: 'projets', component: ProjetsComponent },
+  {path: 'resume', component: ResumeComponent },
+  {path: 'site', component: WebsitesComponent },
+  {path: 'services', component: ServicesComponent },
+  {path: '**', redirectTo: '', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { MainComponent } from './components/main/main.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { WebsitesComponent } from './pages/websites/websites.component';
+import { ProjetsComponent } from './pages/projets/projets.component';
+import { ResumeComponent } from './pages/resume/resume.component';
+import { ServicesComponent } from './pages/services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    MainComponent,
+    FooterComponent,
+    AboutComponent,
+    ContactsComponent,
+    WebsitesComponent,
+    ProjetsComponent,
+    ResumeComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
